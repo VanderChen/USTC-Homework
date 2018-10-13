c0cd1bbecc3cdecaf7aba1eac055656e5f9a2ba8
VanderChen <vanderchen@outlook.com>
2018-10-13 18:58:42 +0800

    Add python homework 2

--- Python/Homework1.py
+++ Python/Homework1.py
@@ -1,17 +1,30 @@
-import math
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = "SA18225036 Min Chen"
+__email__ = "mchen330@mail.ustc.edu.cn"
 
+import math
 
 def main():
     # Get the radiu
     r_str = input("Please input the radius of a sphere\n")
+
     # Check the legal input and caculate
     try:
+        # Transfor input str to float
         r = float(r_str)
-        v = 4 / 3 * math.pi * math.pow(r, 3)
-        a = 4 * math.pi * math.pow(r, 2)
-        print("The valume is : ", v, "The surface is : ", a)
+        # Check positive number
+        if r >= 0:
+            v = 4 / 3 * math.pi * math.pow(r, 3)
+            a = 4 * math.pi * math.pow(r, 2)
+            print("The valume is : ", v, "The surface is : ", a)
+        else:
+            print("Invalid Input! Please input a positive number")
+            # If not legal input ,recall main
+            main()
     except Exception as _:
-        print("Please input a float number")
+        print("Invalid Input! Please input a float number")
+        # If not legal input ,recall main
         main()
 
 
---
+++ Python/Homework2/chaos.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+def calc_chaos(init_val_a, init_val_b, iterations):
+    # open file and calc the chaos number
+    with open("result_for_chaos.txt",'w') as f:
+        f.write("index   " + str(init_val_a) + "    " + str(init_val_b) + "\n")
+        f.write("---------------------" + "\n")
+        for i in range(iterations):
+            # calc the num
+            init_val_a = 3.9 * init_val_a * (1 - init_val_a)
+            init_val_b = 3.9 * init_val_b * (1 - init_val_b)
+            # output the result in file
+            f.write(str(i + 1) + "    " + str(init_val_a) + "    " + str(init_val_b) + "\n")
+        f.close()
+
+def main():
+    # get interations number and check ,ensure the data type
+    try:
+        iterations = int(eval(input("Enter a number for iterations:  ")))
+        init_val_a = float(eval(input("Enter a number for init_val_a:  ")))
+        init_val_b = float(eval(input("Enter a number for init_val_b:  ")))
+    except Exception as e:
+        print("Please input valid number. Error is ",e)
+        main()
+    # call the function to caculate
+    calc_chaos(init_val_a, init_val_b, iterations)
+
+if __name__ == '__main__':
+    main()
