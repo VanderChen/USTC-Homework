ba255212f4e3ef94e604bdbf779af07c4917614c
VanderChen <vanderchen@outlook.com>
2018-10-29 22:29:53 +0800

    Add Python Homework4 Part2

--- Python/Homework4/gpa/gpa.py
+++ Python/Homework4/gpa/gpa.py
@@ -1,27 +1,25 @@
 from student import Student
 
-def readStudents(filename):
-    """return a List of Student object"""
-    infile = open(filename,'r')
-    stulist = []
-    for line in infile:
-        stulist.append(Student.makeStudent(line))
-    infile.close()
-    return stulist
+def readStudents(data_filename):
+    with open(data_filename,'r') as data_file:
+        student_list = []
+        for line in data_file:
+            student_list.append(Student.makeStudent(line))
+        data_file.close()
+        return student_list
 
-def writeStudents(stus,filename):
-    """output the data to a file"""
-    outfile = open(filename, 'w')
-    for s in stus:
-        print("{0:<15}{1:<10}{2:<10}{3:.2f}".format(s.name,s.getHours(),s.getPoints(),s.gpa()),file = outfile)
-    outfile.close()
+def writeStudents(student_list,result_filename):
+    with open(result_filename, 'w') as result_file:
+        for s in student_list:
+            print("{0:<15}{1:<10}{2:<10}{3:.2f}".format(s.getName(),s.getHours(),s.getPoints(),s.gpa()),file = result_file)
+        result_file.close()
 
 def main():
-    data_filename = input("filename:")
-    data = readStudents(data_filename)
-    data.sort(key=Student.gpa,reverse=True) # descending
-    result_filename = input("outfile:")
-    writeStudents(data,result_filename)
+    data_filename = input("Please input the data file name:")
+    student_list = readStudents(data_filename)
+    student_list.sort(key=Student.gpa,reverse=True) # descending
+    result_filename = input("Please input the result file name:")
+    writeStudents(student_list,result_filename)
     pass
 
 if __name__ == '__main__':
